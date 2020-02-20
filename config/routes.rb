170f47f8a7cefaf166b06cdf4dd47ef5ffed1d23
Hiroharu Hara <hh.25799623@gmail.com>
2020-02-20 21:17:52 +0900

    routeの定義を変更(2回目)

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root "posts#index"
   resources :posts
   get 'test', to: "posts#index"
+  get 'test2', to: "posts#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
