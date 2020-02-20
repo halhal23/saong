67b609bdfb5fd2faa33b98473b8ff52f88977c8d
Hiroharu Hara <hh.25799623@gmail.com>
2020-02-20 22:49:30 +0900

    routeの設定変更(4回目)

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :posts
   get 'test', to: "posts#index"
   get 'test2', to: "posts#index"
+  get 'test3', to: "posts#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
